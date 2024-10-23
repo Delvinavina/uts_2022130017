a016aabba7053bfd055ce877611b3fdfd84fc77d
Delvina <vinadelvina2001@gmail.com>
2024-10-23 08:55:19 +0700

    maintheme

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,54 @@
-import 'package:flutter/material.dart';
 import 'package:uts_2022130017/presentation/pages/login.dart';
+import 'package:uts_2022130017/presentation/pages/products.dart';
+import 'package:uts_2022130017/presentation/provider/cart/bloc/cart_bloc.dart';
 import 'package:uts_2022130017/presentation/provider/login/bloc/login_bloc.dart';
+import 'package:uts_2022130017/presentation/provider/product/bloc/product_bloc.dart';
+import 'package:uts_2022130017/theme.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  HydratedBloc.storage = await HydratedStorage.build(
+    storageDirectory: kIsWeb
+        ? HydratedStorage.webStorageDirectory
+        : await getTemporaryDirectory(),
+  );
+  runApp(const MainApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Nekoshop',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => LoginBloc(),
+        ),
+        BlocProvider(
+          create: (context) => ProductBloc()..add(LoadProducts()),
+        ),
+        BlocProvider(
+          create: (context) => CartBloc(),
+        )
+      ],
+      child: MaterialApp(
+        theme: const AppTheme().light(),
+        home: BlocBuilder<LoginBloc, LoginState>(
+          builder: (context, state) {
+            if (state.isLoggedIn) {
+              return ProductsPage();
+            } else {
+              return LoginPage();
+            }
+          },
+        ),
       ),
-      home: const LoginPage(title: 'NekoShop'),
     );
   }
 }
